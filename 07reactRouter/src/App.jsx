6b274aef57b3225d45124964086c436ebe36ecf3
Ganu1415 <mr.vga1415@gmail.com>
2024-04-10 14:50:45 +0530

    create react router dom crash project

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">React Router </h1>
+      <Header />
+    </>
+  );
+}
+
+export default App;
